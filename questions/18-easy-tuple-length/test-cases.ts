ff33eededff63925a879e8876e6c3a5c71e7800a
Eddy Hernandez <eddyhdzg@gmail.com>
2022-04-20 20:41:39 -0500

    :sparkles: easy 14,18,43,189

--- questions/18-easy-tuple-length/test-cases.ts
+++ questions/18-easy-tuple-length/test-cases.ts
@@ -1,7 +1,14 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils"
+import { Length } from "./solution"
 
-const tesla = ['tesla', 'model 3', 'model X', 'model Y'] as const
-const spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT'] as const
+const tesla = ["tesla", "model 3", "model X", "model Y"] as const
+const spaceX = [
+  "FALCON 9",
+  "FALCON HEAVY",
+  "DRAGON",
+  "STARSHIP",
+  "HUMAN SPACEFLIGHT",
+] as const
 
 type cases = [
   Expect<Equal<Length<typeof tesla>, 4>>,
@@ -9,5 +16,5 @@ type cases = [
   // @ts-expect-error
   Length<5>,
   // @ts-expect-error
-  Length<'hello world'>,
+  Length<"hello world">
 ]
--- questions/189-easy-awaited/test-cases.ts
+++ questions/189-easy-awaited/test-cases.ts
@@ -1,4 +1,10 @@
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils"
+
+type MyAwaited<T extends Promise<unknown>> = T extends Promise<infer K>
+  ? K extends Promise<unknown>
+    ? MyAwaited<K>
+    : K
+  : undefined
 
 type X = Promise<string>
 type Y = Promise<{ field: number }>
@@ -7,7 +13,7 @@ type Z = Promise<Promise<string | number>>
 type cases = [
   Expect<Equal<MyAwaited<X>, string>>,
   Expect<Equal<MyAwaited<Y>, { field: number }>>,
-  Expect<Equal<MyAwaited<Z>, string | number>>,
+  Expect<Equal<MyAwaited<Z>, string | number>>
 ]
 
 // @ts-expect-error
